d9c23b06bb8276d3b3560fc13b70a6e224d00412
Subhajit Sahu <wolfram77@gmail.com>
2023-11-30 00:57:07 +0530

    :bug: update utility headers

--- inc/_iostream.hxx
+++ inc/_iostream.hxx
@@ -1,25 +1,17 @@
 #pragma once
-#include <ctime>
-#include <type_traits>
 #include <utility>
+#include <type_traits>
 #include <iterator>
 #include <array>
 #include <vector>
-#include <string>
-#include <istream>
 #include <ostream>
 #include <iostream>
 #include <chrono>
-
-#ifdef OPENMP
-#include <omp.h>
-#endif
+#include <ctime>
 
 using std::pair;
 using std::array;
 using std::vector;
-using std::string;
-using std::istream;
 using std::ostream;
 using std::is_fundamental;
 using std::iterator_traits;
@@ -33,53 +25,13 @@ using std::cout;
 
 
 
-// READ LINES
-// ----------
-
+#pragma region WRITE
 /**
- * Read lines from a stream and apply a function to each line.
- * @param s input stream
- * @param fp process function (line_index, line)
+ * Write values to a stream.
+ * @param a the stream
+ * @param ib begin iterator of values
+ * @param ie end iterator of values
  */
-template <class FP>
-inline void readLinesDo(istream& s, FP fp) {
-  string line;
-  for (size_t l=0; getline(s, line);) {
-    if (line[0]=='#') continue;
-    fp(l++, line);
-  }
-}
-
-
-#ifdef OPENMP
-template <class FP>
-inline void readLinesOmpDo(istream& s, FP fp) {
-  const int LINES = 131072;
-  vector<string> lines(LINES);
-  for (size_t l=0;;) {
-    // Read several lines from the stream.
-    int READ = 0;
-    for (int i=0; i<LINES;) {
-      if (!getline(s, lines[i])) break;
-      if (lines[i][0]=='#') continue;
-      ++i; ++READ;
-    }
-    if (READ==0) break;
-    // Process lines using multiple threads.
-    #pragma omp parallel for schedule(dynamic, 1024)
-    for (int i=0; i<READ; ++i)
-      fp(l+i, lines[i]);
-    l += READ;
-  }
-}
-#endif
-
-
-
-
-// WRITE
-// -----
-
 template <class I>
 inline void write_values(ostream& a, I ib, I ie) {
   using T = typename iterator_traits<I>::value_type;
@@ -96,43 +48,95 @@ inline void write_values(ostream& a, I ib, I ie) {
     a << "}";
   }
 }
+
+
+/**
+ * Write values to a stream.
+ * @param a the stream
+ * @param x the container of values
+ */
 template <class J>
 inline void writeValues(ostream& a, const J& x) {
   write_values(a, x.begin(), x.end());
 }
 
+
+/**
+ * Write a pair to a stream.
+ * @param a the stream
+ * @param x the pair
+ */
 template <class K, class V>
 inline void write(ostream& a, const pair<K, V>& x) {
   a << x.first << ": " << x.second;
 }
+
+
+/**
+ * Write an array to a stream.
+ * @param a the stream
+ * @param x the array
+ */
 template <class T, size_t N>
 inline void write(ostream& a, const array<T, N>& x) {
   writeValues(a, x);
 }
+
+
+/**
+ * Write a vector to a stream.
+ * @param a the stream
+ * @param x the vector
+ */
 template <class T>
 inline void write(ostream& a, const vector<T>& x) {
   writeValues(a, x);
 }
 
+
+/**
+ * Write a pair to a stream.
+ * @param a the stream
+ * @param x the pair
+ */
 template <class K, class V>
 inline ostream& operator<<(ostream& a, const pair<K, V>& x) {
   write(a, x); return a;
 }
+
+
+/**
+ * Write an array to a stream.
+ * @param a the stream
+ * @param x the array
+ */
 template <class T, size_t N>
 inline ostream& operator<<(ostream& a, const array<T, N>& x) {
   write(a, x); return a;
 }
+
+
+/**
+ * Write a vector to a stream.
+ * @param a the stream
+ * @param x the vector
+ * @returns the stream
+ */
 template <class T>
 inline ostream& operator<<(ostream& a, const vector<T>& x) {
   write(a, x); return a;
 }
+#pragma endregion
 
 
 
 
-// WRITE TIME
-// ----------
-
+#pragma region WRITE TIME
+/**
+ * Write a time to a stream.
+ * @param a the stream
+ * @param x the time
+ */
 inline void writeTime(ostream& a, const time_t& x) {
   const int BUF = 64;
   char  buf[BUF];
@@ -147,25 +151,68 @@ inline void writeTime(ostream& a, const time_t& x) {
   );
   a << buf;
 }
+
+
+/**
+ * Write a time point to a stream.
+ * @param a the stream
+ * @param x the time point
+ */
 inline void writeTimePoint(ostream& a, const time_point<system_clock>& x) {
   writeTime(a, system_clock::to_time_t(x));
 }
 
+
+/**
+ * Write a time to a stream.
+ * @param a the stream
+ * @param x the time
+ * @returns the stream
+ */
 inline ostream& operator<<(ostream& a, const time_t& x) {
   writeTime(a, x); return a;
 }
+
+
+/**
+ * Write a time point to a stream.
+ * @param a the stream
+ * @param x the time point
+ * @returns the stream
+ */
 inline ostream& operator<<(ostream& a, const time_point<system_clock>& x) {
   writeTimePoint(a, x); return a;
 }
+#pragma endregion
 
 
 
 
-// PRINT*
-// ------
-
+#pragma region PRINT
+/**
+ * Print an object.
+ * @param x the object to print
+ */
 template <class T>
-inline void print(const T& x)   { cout << x; }
+inline void print(const T& x) {
+  cout << x;
+}
+
+
+/**
+ * Print an object followed by a newline.
+ * @param x the object to print
+ */
 template <class T>
-inline void println(const T& x) { cout << x << "\n"; }
-inline void println()           { cout << "\n"; }
+inline void println(const T& x) {
+  cout << x << "\n";
+}
+
+
+/**
+ * Print a newline.
+ */
+inline void println() {
+  cout << "\n";
+}
+#pragma endregion
